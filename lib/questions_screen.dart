f1540c7207f75fe37a5b11636fd2faf13ffad249
daniel-liberto <daniel.liberto@hotmail.com>
2023-06-21 16:56:04 -0300

    the app now are already consuming the questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,28 +12,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Question 1',
-            style: TextStyle(color: Colors.white, fontSize: 30),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer 1',
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer 2',
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer 3',
-          ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(onTap: () {}, answerText: answer);
+          })
         ],
       ),
     );
